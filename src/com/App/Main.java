2a62e84b1be2e6bc41a530bf77d41a51969ed31f
Saiprasaddabbe <dabbesaiprasad8@gmail.com>
2022-09-29 21:15:31 +0530

    aa

--- src/com/App/Main.java
+++ src/com/App/Main.java
@@ -6,9 +6,6 @@ public class Main {
 
 	public static void main(String[] args) {
 
-		
-		
-		
 		Scanner sc = new Scanner(System.in);
 		
 		System.out.println("Welcome to the Online Banking System:");
